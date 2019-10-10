71eb1267593d08c2ec063b507ae47d8121a9a196
anawiese <ana.wieset@gmail.com>
2019-10-10 20:53:37 -0300

    CRUD Scaffolding

---
+++ SalesWbMvc/Data/SalesWbMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWbMvc.Models
+{
+    public class SalesWbMvcContext : DbContext
+    {
+        public SalesWbMvcContext (DbContextOptions<SalesWbMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWbMvc.Models.Department> Department { get; set; }
+    }
+}
